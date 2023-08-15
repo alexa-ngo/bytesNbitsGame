6a382da0cfab86ad835bf0a00e5f4caf8bb143a1
Alexa Ngo <alexaariango@outlook.com>
2023-08-15 07:32:34 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bytesNbitsGame
+# BytesNbits Game
+
+Quick! This game checks to see how fast a user can convert their bites, bytes, KB, MB, and TB!
--- bytesNbits.js
+++ bytesNbits.js
@@ -1,6 +1,29 @@
 function bytesNBitsGame(num) {
-    console.log(num)
+
+    for (let i = 0; i < 10; i++) {
+        console.log(num)
+    }
 }
 
-const num = 8
-console.log(bytesNBitsGame(num))
+
+
+
+
+
+function startTime() {
+    let start = Date.now();
+    return start
+};
+
+function endTime(startTime) {
+    let timeTaken = Date.now() - startTime;
+    return `Total time taken : ${timeTaken} milliseconds`
+    //    return 'Total time taken, $`timeTaken` + " milliseconds"'
+};
+
+const start = startTime();
+const num = 4
+bytesNBitsGame(num);
+console.log(endTime(start));
+
+
