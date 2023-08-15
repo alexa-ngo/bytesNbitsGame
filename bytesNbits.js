e58f730e439928d235f1b16fa6e87670352391d1
Alexa Ngo <alexaariango@outlook.com>
2023-08-15 08:07:41 -0700

    Progress

--- bytesNbits.js
+++ bytesNbits.js
@@ -1,8 +1,46 @@
-function bytesNBitsGame(num) {
+function bytesNBitsGame(numOfQuestions) {
 
-    for (let i = 0; i < 10; i++) {
-        console.log(num)
+    const start = startTime()
+
+    let score = 0;
+    let answerInt = 0;
+    let inputUnit = null;
+
+    // Generate integer between 1-128
+    const randomInt = randomIntFromInterval()
+
+    // Generate a random question unit: Bits, B, KB, MB, GB, TB
+
+    for (let i = 1; i < 4; i++) {
+        const unit = randomUnit();
+        //console.log(i, unit)
+
+        if (unit === 'bits') {
+            inputUnit = 'B';
+            answerInt = randomInt / 8
+            return unit, inputUnit
+
+        } else if (unit === 'TB') {
+
+            inputUnit = 'GB';
+            return unit, inputUnit
+        } else if (unit === 'KB') {
+
+            inputUnit = 'B'
+            // const unitArr = ['B', 'MB']
+            //return unitArr[Math.floor(Math.random() * unitArr.length)]
+            return unit, inputUnit
+        } else if (unit === 'MB') {
+            inputUnit = 'KB'
+            return unit, inputUnit
+
+        } else if (unit === 'GB') {
+            inputUnit = 'MB'
+            return unit, inputUnit
+        }
+
     }
+    console.log(endTime(start))
 }
 
 
@@ -18,12 +56,21 @@ function startTime() {
 function endTime(startTime) {
     let timeTaken = Date.now() - startTime;
     return `Total time taken : ${timeTaken} milliseconds`
-    //    return 'Total time taken, $`timeTaken` + " milliseconds"'
 };
 
-const start = startTime();
+function randomIntFromInterval() {
+    return Math.floor(Math.random() * (128) + 1)
+}
+
+function randomUnit() {
+    const unitArr = ['bits', 'B', 'KB', 'MB', 'GB', 'TB']
+    return unitArr[Math.floor(Math.random() * unitArr.length)]
+};
+
+//console.log(randomUnit())
+
+
 const num = 4
-bytesNBitsGame(num);
-console.log(endTime(start));
+console.log(bytesNBitsGame(num));
 
 
